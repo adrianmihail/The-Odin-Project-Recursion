9ecf38e5b30a85d02b4efa24ba0578a410eee523
adrianmihail <adrianmihail00@gmail.com>
2024-06-06 12:22:32 +0300

    Removed old code
    Done fibonacci with iteration

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,25 +1,15 @@
-def fibs(num)
-  # Error check
-  if num < 0 then
-      raise ArgumentError, "The number must be a positive integer"
+def fibs(index)
+  sequence = [0,1,1]
+  index_location = index - 1
+  counter = 0
+
+  while counter < index_location
+    new_number = sequence[-2] + sequence[-1]
+    sequence.append(new_number)
+    counter += 1
   end
 
-  # Terminating base cases
-  return [] if num == 0
-  return [0] if num == 1
-  return [0,1] if num == 2 
-
-  # Recursion
-  seq = fibs(num - 1) 
-
-  # The recursive function
-  seq << seq[-2] + seq[-1]
-
-  return seq
+  sequence[index_location]
 end
 
-p fibs(0)
-p fibs(1)
-p fibs(2)
-p fibs(4)
 p fibs(8)
