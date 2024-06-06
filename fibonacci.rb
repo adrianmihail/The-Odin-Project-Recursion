d1cbd3900912ae4656a45d7e78e992338618bdd0
adrianmihail <adrianmihail00@gmail.com>
2024-06-06 13:19:27 +0300

    Fixed Iteration
    Created Recursion Method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,7 +1,7 @@
 def fibs(index)
   sequence = [0,1,1]
   index_location = index - 1
-  counter = 0
+  counter = 2
 
   while counter < index_location
     new_number = sequence[-2] + sequence[-1]
@@ -9,7 +9,20 @@ def fibs(index)
     counter += 1
   end
 
-  sequence[index_location]
+  sequence
 end
 
 p fibs(8)
+
+def fibs_rec(index, n1 = 1, n2 = 1, sequence = [0,1,1])
+
+  return sequence if sequence.length() == index
+  return sequence[0..0] if index == 1 
+  return sequence[0..1] if index == 2
+  return sequence[0..2] if index == 3
+  
+  fibs_rec(index, n1 = sequence[-2], n2 = sequence[-1], sequence.append(n2+n1))
+
+end
+
+p fibs_rec(8)
